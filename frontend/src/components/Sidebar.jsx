a231ed6232638f50dccaa2a96a0ec1ec76efc24e
Shubham Jalwaniya <jalwaniyash702@gmail.com>
2025-08-28 11:34:18 +0530

    initial commit with authentication and some pages are completed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
-import { PRODUCTIVITY_CARD, SIDEBAR_CLASSES } from '../assets/dummy'
-import { Sparkles } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import { LINK_CLASSES, menuItems, PRODUCTIVITY_CARD, SIDEBAR_CLASSES, TIP_CARD } from '../assets/dummy'
+import { Lightbulb, Menu, Sparkles, X } from 'lucide-react';
+import { NavLink } from 'react-router-dom';
 
 
 const Sidebar = ({user, tasks}) => {
@@ -16,8 +17,34 @@ const Sidebar = ({user, tasks}) => {
   const username = user?.name || "User";
   const initial = username[0].charAt(0).toUpperCase();
 
+
+  useEffect(() => {
+    document.body.style.overflow = mobileOpen ? "hidden" : "auto"
+    return () => { document.body.style.overflow = "auto" }
+
+  }, [mobileOpen])
+
+  const renderMenuItems = (isMobile = false) =>(
+    <ul className='space-y-2'>
+      {menuItems.map(({text, path, icon}) => (
+        <li key={text}>
+          <NavLink to={path} className={({isActive}) =>[
+            LINK_CLASSES.base, isActive ? LINK_CLASSES.active : LINK_CLASSES.inactive,
+            isMobile ? "justify-start" : "lg:justify-start"
+          ].join(" ")}  onClick={() => setMobileOpen(false)}>
+            <span className={LINK_CLASSES.icon}>{icon}</span>
+            <span className={`${isMobile ? "block" : "hidden lg:block"} ${LINK_CLASSES} `}> {text}</span>
+
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  )
+  
+
   return (
     <>
+    {/* Desktop Sidebar */}
     <div className={SIDEBAR_CLASSES.desktop}>
      <div className='p-5 border-b border-purple-100 lg:block hidden'>
       <div className='flex items-center gap-3'>
@@ -25,8 +52,7 @@ const Sidebar = ({user, tasks}) => {
           {initial}
         </div>
 
-        <div>
-
+      <div>
       <h2 className='text-lg font-bold text-gray-800'> Hey, {username} 🐞</h2>
       <p className='text-sm text-purple-500 font-medium flex items-center gap-2'>
         <Sparkles className='size-4' /> Let's crush some tasks!😎
@@ -40,18 +66,75 @@ const Sidebar = ({user, tasks}) => {
        <div className={PRODUCTIVITY_CARD.header}>
         <h3 className={PRODUCTIVITY_CARD.label}>PRODUCTIVITY</h3>
         <span className={PRODUCTIVITY_CARD.badge}>{productivity}%</span>
-
        </div>
 
+       <div className={PRODUCTIVITY_CARD.barBg}>
+        <div className={PRODUCTIVITY_CARD.barFg}
+         style={{ width: `${productivity}%`}}
+        />
+       </div>
       </div>
 
+      {renderMenuItems()}
+
+      {/* this is not important so you can skip it */}
+      <div className='mt-auto pt-6 lg:block hidden'>
+       <div className={TIP_CARD.container}>
+        <div className='flex items-center  gap-3'>
+          <div className={TIP_CARD.iconWrapper}>
+           <Lightbulb className='size-5 text-purple-600' />
+          </div>
+          <h3 className={TIP_CARD.title}>Pro Tip</h3>
+          <p className={TIP_CARD.text}>Use keyboard shortcuts to boost productivity</p>
+          {/* <a href="https://hexagondigitalservices.com" target='_blank' className='block mt-2 text-sm text-purple-500 hover:underline'>
+          Visit Hexagon Digital Services</a> */}
+        </div>
+       </div>
+      </div>
      </div>
     </div>
 
+    {/* MOBILE MENU */}
+    {!mobileOpen && (
+      <button onClick={() => setMobileOpen(true)} 
+      className={SIDEBAR_CLASSES.mobileButton}>
+        <Menu className='size-5' />
+      </button>
+    )}
+
+    {/* MOBILE DROWER */}
+    {mobileOpen && (
+      <div className='fixed inset-0 z-40'>
+       <div className={SIDEBAR_CLASSES.mobileDrawerBackdrop} onClick={() => setMobileOpen(false)} />
+        <div className={SIDEBAR_CLASSES.mobileDrawer} onClick={(e) => e.stopPropagation()}>
+          <div className='flex items-center justify-between mb-4 border-b pb-2 '>
+           <h2 className='txt-lg font-bold text-purple-600'>Menu</h2>
+           <button onClick={() => setMobileOpen(false)} className='text-gray-700 hover:text-purple-600'>
+            <X className="size-5" />
+           </button>
+          </div>
+
+        <div className='flex items-center gap-3 mb-6'>
+         <div className='size-10 rounded-full bg-gradient-to-br from-fuchsia-500 to-purple-600 flex items-center justify-center text-white font-bold shadow-md'>
+          {initial}
+         </div>
+
+          <div>
+           <h2 className='mt-16 text-lg font-bold text-gray-800'> Hey,   {username} 🐞</h2>
+           <p className='text-sm text-purple-500 font-medium flex items-center gap-2'>
+           <Sparkles className='size-4' /> Let's crush some  tasks!😎
+           </p>
+          </div>
+         </div>
+
+         {renderMenuItems(true)}
+        </div>
+       </div>
+     )}
     </>
   )
 }
 
 export default Sidebar
 
-// 3 : 00
+// 3 : 11
